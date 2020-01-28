eddbb5f15de1243282aaf33fc6a66db80f3beae5
Fahim Ali Zain <faztp12@gmail.com>
2020-01-28 08:38:39 +0530

    Removed CORS

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ import { log } from "./logging";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({
-    origin: true
-  });
   const port = process.env.PORT || 3000;
   log(`Starting on Port ${port}`);
   await app.listen(process.env.PORT || 3000);
