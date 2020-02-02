ad3d6f99497e592bf378b581671e73f2c33f7759
Fahim <faztp12@gmail.com>
2020-02-02 10:05:46 +0530

    Removed Nestjs

--- src/book/author/author.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller("author")
-export class AuthorController {}
--- src/book/genre/genre.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, PrimaryColumn } from "typeorm";
-
-@Entity()
-export class Genre {
-  @PrimaryColumn()
-  type: string;
-}
