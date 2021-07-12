0887b4e0381319db44dbe5932d3f31d8e51365ce
Fahim Ali Zain <fahimalizain@gmail.com>
2021-07-12 19:03:08 +0530

    fix: auto check default for Entity Media

--- thecreamind_frappe/the_crea_mind/doctype/book/book.py
+++ thecreamind_frappe/the_crea_mind/doctype/book/book.py
@@ -5,6 +5,23 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
+
 
 class Book(Document):
-	pass
+    def validate(self):
+        self.validate_media()
+
+    def validate_media(self):
+        if not len(self.get("media") or []):
+            return
+
+        if any(cint(x.get("default")) for x in self.get("media")):
+            return
+
+        sorted_media = sorted(
+            [x for x in self.get("media") if not cint(x.get("disabled"))],
+            key=lambda a: 0 if a.get('type') == "Image" else 1)
+
+        if len(sorted_media):
+            sorted_media[0].default = 1
