060f0ab07fd153ddaca4b9780df5a19d17b5951e
Fahim Ali Zain <fahimalizain@gmail.com>
2021-11-13 07:50:27 +0530

    feat: Registration Dialogs

--- thecreamind_frappe/the_crea_mind/doctype/registration_request/registration_request.js
+++ thecreamind_frappe/the_crea_mind/doctype/registration_request/registration_request.js
@@ -23,9 +23,7 @@ frappe.ui.form.on('Registration Request', {
 			})
 		}, 'Update');
 
-		frappe.db.get_doc("Crea Mind Settings").then(r => {
-			thecreamind.settings = r;
-		})
+
 	},
 
 	show_plan_selection_dialog(frm) {
@@ -60,9 +58,10 @@ frappe.ui.form.on('Registration Request', {
 		d.show();
 	},
 
-	show_plan_confirmation_dialog(frm, args) {
+	async show_plan_confirmation_dialog(frm, args) {
 		const plan = thecreamind.selected_plan;
-		console.log("ARGSS", thecreamind.selected_plan)
+
+		const caution_deposit = await thecreamind.calculate_caution_deposit(plan.has_activity_access, plan.has_library_access);
 		const endDate = frappe.datetime.add_days(plan.start_date, plan.plan_duration);
 		const d = new frappe.ui.Dialog({
 			title: 'Approve Registration Request',
@@ -70,20 +69,66 @@ frappe.ui.form.on('Registration Request', {
 				// Summary
 				{ fieldname: "membership_plan", label: "Membership Plan", fieldtype: "Link", options: "Membership Plan", read_only: 1, default: plan.name },
 				{ fieldname: "start_date", label: "Start Date", fieldtype: "Date", read_only: 1, default: plan.start_date },
-				{ fieldname: "plan_duration", label: "Plan Duration", fieldtype: "Data", read_only: 1, default: plan.plan_duration + " Days"},
+				{ fieldname: "plan_duration", label: "Plan Duration", fieldtype: "Data", read_only: 1, default: plan.plan_duration + " Days" },
 
 				{ fieldname: "cb_1", label: "", fieldtype: "Column Break" },
 				{ fieldname: "has_library_access", label: "Has Library Access", fieldtype: "Check", read_only: 1, default: plan.has_library_access },
 				{ fieldname: "has_activity_access", label: "Has Activity Access", fieldtype: "Check", read_only: 1, default: plan.has_activity_access },
 				{ fieldname: "plan_amount", label: "Plan Amount", fieldtype: "Currency", read_only: 1, default: plan.amount },
-				
+
 				// To Fill
 				{ fieldname: "sb_1", label: "Please fill the following", fieldtype: "Section Break" },
 				{ fieldname: "end_date", label: "End Date", fieldtype: "Date", reqd: 1, default: endDate },
-				{ fieldname: "caution_deposit", label: "Caution Deposit", fieldtype: "Currency", reqd: 1 },
-				{ fieldname: "membership_fee", label: "Membership Fee", fieldtype: "Currency", reqd: 1, fetch_from: "membership_plan.amount", default: plan.amount },
-			]
+				{ fieldname: "caution_deposit_collected", label: "Caution Deposit Collected", fieldtype: "Currency", reqd: 1, default: caution_deposit },
+				{ fieldname: "membership_amount", label: "Membership Amt Collected", fieldtype: "Currency", reqd: 1, fetch_from: "membership_plan.amount", default: plan.amount },
+			],
+			primary_action_label: "Approve",
+			primary_action(values) {
+				frm.call("approve_membership", {
+					...values
+				}).then((r) => {
+					if (r.exc) {
+						frappe.msgprint("Something went wrong");
+						console.error(r);
+					} else if (r.message.status === "SUCCESS") {
+						const { status, kid, kid_membership } = r.message;
+						frappe.msgprint(`
+						Registration Successful!
+						<hr/>
+						Crea Mind Kid: <a href="/app/crea-mind-kid/${kid}">${kid}</a><br/>
+						Kid Membership: <a href="/app/kid-membership/${kid_membership}">${kid_membership}</a>
+						`);
+
+						d.hide();
+						frm.reload_doc();
+						console.log(r.message);
+					}
+				})
+			}
 		});
 		d.show();
 	}
 });
+
+
+// TODO: Move to general file
+thecreamind.calculate_caution_deposit = async (has_activity_access, has_library_access) => {
+	await thecreamind.load_settings();
+	if (has_library_access) {
+		return thecreamind.settings.library_caution_deposit;
+	}
+	if (has_activity_access) {
+		return thecreamind.settings.activity_caution_deposit;
+	}
+}
+
+thecreamind.load_settings = () => new Promise(res => {
+	if (thecreamind.settings) {
+		res(thecreamind.settings);
+	} else {
+		frappe.db.get_doc("Crea Mind Settings").then(r => {
+			thecreamind.settings = r;
+			res(r);
+		})
+	}
+})
--- thecreamind_frappe/the_crea_mind/doctype/registration_request/registration_request.py
+++ thecreamind_frappe/the_crea_mind/doctype/registration_request/registration_request.py
@@ -1,8 +1,45 @@
 # Copyright (c) 2021, Fahim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class RegistrationRequest(Document):
-	pass
+
+    @frappe.whitelist()
+    def approve_membership(self, args):
+        kid = self.as_dict()
+        kid.update(dict(
+            doctype="Crea Mind Kid",
+            name=None, __islocal=True,
+        ))
+        kid = frappe.get_doc(kid)
+        kid.insert(ignore_permissions=True)
+
+        membership = frappe.get_doc(dict(
+            doctype="Kid Membership",
+            kid=kid.name, membership_plan=args.get("membership_plan"),
+            caution_deposit_collected=args.get("caution_deposit_collected"),
+            membership_amount=args.get("membership_amount"),
+
+            start_date=args.get("start_date"), end_date=args.get("end_date"),
+        )).insert(ignore_permissions=True)
+
+        caution_deposit = frappe.get_doc(dict(
+            doctype="Caution Deposit Entry",
+            kid=kid.name, type="Add",
+            ref_doctype="Kid Membership", ref_docname=membership.name,
+            amount=args.get("caution_deposit_collected"), docstatus=1,
+        )).insert(ignore_permissions=True)
+
+        self.kid = kid.name
+        self.status = "Approved"
+        self.save(ignore_permissions=True)
+
+        return dict(
+            status="SUCCESS",
+            kid=kid.name,
+            kid_membership=membership.name,
+            caution_deposit=caution_deposit.name,
+        )
